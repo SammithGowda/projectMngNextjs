9e349a84c22cb9670ed96d411664f949c82c4917
sam <hsammith@gmail.com>
2025-01-01 15:33:09 +0530

    added home navbar

--- src/(components)/Sidebar/index.tsx
+++ src/(components)/Sidebar/index.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { LockIcon } from "lucide-react";
+import { Home, LockIcon, LucideIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "src/app/redux";
 
 const Sidebar = () => {
   const [showProject, setShowProject] = useState(true);
@@ -27,9 +30,52 @@ const Sidebar = () => {
             </div>
           </div>
         </div>
+        {/* NAVBBAR LINKS */}
+        <nav className="z-10 w-full">
+          <SideBarLink
+            icon={Home}
+            href="/"
+            lable="Home"
+          />
+        </nav>
       </div>
     </div>
   );
 };
+
+interface SideBarLinkProps{
+  href:string,
+  icon:LucideIcon,
+  lable:string,
+  // isCollapsed:boolean
+}
+const SideBarLink = ({
+  href,
+  icon:Icon,
+  lable,
+  // isCollapsed
+}:SideBarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === '/' && href === '/dashboard')
+  const screeWidth = window.innerWidth;
+    const dispatch = useAppDispatch()
+    const isSidebarCollapsed = useAppSelector((state)=>state.global.isSidebarCollapsed);
+  return (
+    <Link href={href} className="w-full">
+      <div className={`relative flex cursor-pointer items-center gap-3 transition-colors
+         hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-600 ${
+          isActive?"bg-gray-100 text-white drak:bg-gray-600":""
+         }
+        `}>
+          {isActive && (
+            <div className="left-0 right-0 h-[100%] w-[5px] bg-blue-200">
+            </div>
+            )}
+            <Icon className="h-6' w-6 text-gray-800 dark:text-gray-100"/>
+            <span className='font-medium text-gray-800 dark:text-gray-100'>{lable}</span>
+      </div>
+    </Link>
+  )
+}
 
 export default Sidebar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center border-2 border-green-500 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 dark:bg-dark-bg dark:text-white font-[family-name:var(--font-geist-sans)]">
       hi asosldjzfdasd
-    </div>
+      </div>
   );
 }
