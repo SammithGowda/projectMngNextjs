2811d380e4668278089e9e1907b083ea75029672
sam <hsammith@gmail.com>
2025-01-15 21:18:05 +0530

    removed border

--- src/app/dashboardWraper.tsx
+++ src/app/dashboardWraper.tsx
@@ -16,7 +16,7 @@ const DashboardLayer = ({children}:{children:React.ReactNode}) => {
       }
     })
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900 border-2 border-red-500 '>
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
          <Sidebar/>
         <main className={`flex w-full flex-col bg-gray-50 dark:bg-black-bg ${isSidebarCollapsed?"":"md:pl-64"}`}>
             {/* navbar */}
