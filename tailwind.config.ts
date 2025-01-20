9347d6dc12503c5daf91acc8f11a2be8ed05a028
sam <hsammith@gmail.com>
2025-01-20 20:01:18 +0530

    removed bug of (component)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ export default {
   darkMode:"class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/(components)/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
